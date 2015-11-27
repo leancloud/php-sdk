517de90d41a7cc75d5a07cd2f23a366d8a4a615d
Juvenn Woo <machese@gmail.com>
2015-11-27 16:27:33 +0800

    Add Cloud functions and hooks test

--- src/LeanCloud/Engine/Cloud.php
+++ src/LeanCloud/Engine/Cloud.php
@@ -1,4 +1,5 @@
 <?php
+namespace LeanCloud\Engine;
 
 /**
  * Define functions and hooks on cloud
@@ -20,6 +21,7 @@
  * });
  *```
  */
+
 class Cloud {
 
     /**
@@ -29,49 +31,321 @@ class Cloud {
      */
     private static $repo = array();
 
-    public static function getFunc($funcName) {}
-    public static function getHookName($subject, $hookName) {}
+    /**
+     * Hook map
+     */
+    private static $hookMap = array(
+        "beforeSave"   => "__before_save_for_",
+        "afterSave"    => "__after_save_for_",
+        "beforeUpdate" => "__before_update_for_",
+        "afterUpdate"  => "__after_update_for_",
+        "beforeDelete" => "__before_delete_for_",
+        "afterDelete"  => "__after_delete_for_",
+        "onLogin"      => "__on_login_",
+        "onVerified"   => "__on_verified_",
+        "onComplete"   => "__on_complete_"
+    );
+
+    /**
+     * Get defined function by name
+     *
+     * @param string $funcName Name of function or hook
+     * @return callable|null
+     */
+    public static function getFunc($funcName) {
+        return (isset(self::$repo[$funcName]) ? self::$repo[$funcName] : null);
+    }
+
+    /**
+     * Get internal hook name
+     *
+     * @param string $hookName
+     * @return string
+     */
+    private static function getHookPrefix($hookName) {
+        return (isset(self::$hookMap[$hookName]) ?
+                self::$hookMap[$hookName] : null);
+    }
 
     /**
      * Define a cloud function
      *
-     * The POST body will be json decoded into array, which will be passed
-     * callable as first argument. The current user, if available, will be
-     * passed as second arg. Example:
+     * The function accepts two arguments: the first is an array of
+     * parameters, the second is user in the session. Example:
      *
      * ```php
      * Cloud::define("sayHello", function($params, $user) {
-     *     return "Hello $params['name']!"
+     *     return "Hello {$params['name']}!";
      * });
      * ```
      *
      * @param string   $funcName
-     * @param callable $func The function accepts two arguments
+     * @param callable $func
      */
     public static function define($funcName, $func) {
         self::$repo[$funcName] = $func;
     }
 
     /**
-     * Define after save hook for a class
+     * Define before save hook for a class
      *
+     * The function shall take two arguments: the first one is class
+     * object, the second is user if available in session. If your $func
+     * throws `FunctionError`, the save will be rejected.
      *
-     * @param string   $className
+     * @param string $className
      * @param callable $func
+     * @see FunctionError
      */
-    public static function afterSave($className, $func) {
-        self::define("__afterSave_{$className}", $func);
+    public static function beforeSave($className, $func) {
+        $name = self::getHookPrefix("beforeSave") . $className;
+        self::define($name, $func);
     }
 
     /**
-     * Register hook on user verified email or sms
+     * Define after save hook for a class
      *
-     * @param string   $type Either sms or email
+     * The function shall take two arguments: the first one is class
+     * object, the second is user if available in session. Any error
+     * in after hook will be ignored.
+     *
+     * @param string $className
      * @param callable $func
+     * @see FunctionError
      */
-    public static function onVerified($type, $func) {}
-    public static function onLogin($func) {}
-    public static function onBigQuery($event, $func) {}
-    public static function onInsight($event, $func) {}
+    public static function afterSave($className, $func) {
+        $name = self::getHookPrefix("afterSave") . $className;
+        self::define($name, $func);
+    }
+
+    /**
+     * Define before update hook for a class
+     *
+     * The function shall take two arguments: the first one is class
+     * object, the second is user if available in session. If your $func
+     * throws `FunctionError`, the update will be rejected.
+     *
+     * @param string $className
+     * @param callable $func
+     * @see FunctionError
+     */
+    public static function beforeUpdate($className, $func) {
+        $name = self::getHookPrefix("beforeUpdate") . $className;
+        self::define($name, $func);
+    }
+
+    /**
+     * Define after update hook for a class
+     *
+     * The function shall take two arguments: the first one is class
+     * object, the second is user if available in session. Any error
+     * in $func will be ignored.
+     *
+     * @param string $className
+     * @param callable $func
+     * @see FunctionError
+     */
+    public static function afterUpdate($className, $func) {
+        $name = self::getHookPrefix("afterUpdate") . $className;
+        self::define($name, $func);
+    }
+
+    /**
+     * Define before delete hook for a class
+     *
+     * The function shall take two arguments: the first one is class
+     * object, the second is user if available in session. If your $func
+     * throws `FunctionError`, the delete will be rejected.
+     *
+     * @param string $className
+     * @param callable $func
+     * @see FunctionError
+     */
+    public static function beforeDelete($className, $func) {
+        $name = self::getHookPrefix("beforeDelete") . $className;
+        self::define($name, $func);
+    }
+
+    /**
+     * Define after delete hook for a class
+     *
+     * The function shall take two arguments: the first one is class
+     * object, the second is user if available in session. Any error
+     * in $func will be ignored.
+     *
+     * @param string $className
+     * @param callable $func
+     * @see FunctionError
+     */
+    public static function afterDelete($className, $func) {
+        $name = self::getHookPrefix("afterDelete") . $className;
+        self::define($name, $func);
+    }
+
+    /**
+     * Define hook for when user tries to login
+     *
+     * The function takes one argument, the login user. A `FunctionError`
+     * could be thrown in the $func, which will reject the user for login.
+     *
+     * @param callable $func
+     * @see self::runOnLogin
+     */
+    public static function onLogin($func) {
+        self::define("__on_login__User", $func);
+    }
+
+
+    /**
+     * Define hook for when user verified sms or email
+     *
+     * The function takes one argument, the verified user.
+     *
+     * @param string   $type Either "sms" or "email"
+     * @param callable $func
+     * @see self::runOnVerified
+     */
+    public static function onVerified($type, $func) {
+        self::define("__on_verified_{$type}", $func);
+    }
+
+    /**
+     * Define on complete hook for big query
+     *
+     * @param callable $func
+     * @alias self::onInsight()
+     */
+    public static function onBigQuery($func) {
+        self::onInsight($func);
+    }
+
+    /**
+     * Define on complete hook for big query
+     *
+     * The function takes one argument, the big query job info as array:
+     *
+     * ```php
+     * array(
+     *     "id" => "job id",
+     *     "status" => "OK/ERROR",
+     *     "message" => "..."
+     * );
+     * ```
+     *
+     * @param callable $func
+     * @see self::runOnInsight
+     */
+    public static function onInsight($func) {
+        self::define("__on_complete_bigquery_job", $func);
+    }
+
+    /**
+     * Run cloud function
+     *
+     * Example:
+     *
+     * ```php
+     * LeanEngine::runFunc("sayHello", array("name" => "alice"), $user);
+     * // sayHello(array("name" => "alice"), $user);
+     * ```
+     *
+     * @param string   $funcName Name of defined function
+     * @param array    $data     Array of parameters passed to function
+     * @param LeanUser $user     Request user
+     * @return mixed
+     * @throws FunctionError
+     * @see self::define
+     */
+    public static function runFunc($funcName, $params, $user) {
+        $func = self::getFunc($funcName);
+        if (!$func) {
+            throw new FunctionError("Cloud function not found.", 404);
+        }
+        return call_user_func($func, $params, $user);
+    }
+
+    /**
+     * Run cloud hook
+     *
+     * Example:
+     *
+     * ```php
+     * LeanEngine::runHook("TestObject", "beforeUpdate", $object, $user);
+     * // hook($object, $user);
+     * ```
+     *
+     * @param string $className  Classname
+     * @param string $hookName   Hook name, e.g. beforeUpdate
+     * @param LeanObject $object The object of attached hook
+     * @param LeanUser   $user   Request user
+     * @return mixed
+     * @throws FunctionError
+     */
+    public static function runHook($className, $hookName, $object,
+                                   $user=null) {
+        $name = self::getHookPrefix($hookName) . $className;
+        $func = self::getFunc($name);
+        if (!$func) {
+            throw new FunctionError("Cloud hook `{$name}' not found.",
+                                    404);
+        }
+        return call_user_func($func, $object, $user);
+    }
+
+    /**
+     * Run hook when a user logs in
+     *
+     * @param LeanUser $user The user that tries to login
+     * @throws FunctionError
+     * @see self::onLogin
+     */
+    public static function runOnLogin($user) {
+        return self::runHook("_User", "onLogin", $user);
+    }
+
+    /**
+     * Run hook when user verified by Email or SMS
+     *
+     * @param string   $type Either "sms" or "email", case-sensitive
+     * @param LeanUser $user The verifying user
+     * @throws FunctionError
+     * @see self::onVerified
+     */
+    public static function runOnVerified($type, $user) {
+        $name = "__on_verified_{$type}";
+        $func = self::getFunc($name);
+        if (!$func) {
+            throw new FunctionError("Cloud hook `{$name}' not found.",
+                                    404);
+        }
+        return call_user_func($func, $user);
+    }
+
+    /**
+     * Run hook when BigQuery complete
+     *
+     * @see self::runOnInsight
+     */
+    public static function runOnBigQuery($params) {
+        return self::runOnInsight($params);
+    }
+
+    /**
+     * Run hook on big query complete
+     *
+     * @param array $job Big query job info
+     * @return mixed
+     * @throws FunctionError
+     * @see self::onInsight
+     */
+    public static function runOnInsight($job) {
+        $name = "__on_complete_bigquery_job";
+        $func = self::getFunc($name);
+        if (!$func) {
+            throw new FunctionError("Cloud hook `{$name}' not found.",
+                                    404);
+        }
+        return call_user_func($func, $job);
+    }
 }
 
---
+++ tests/CloudTest.php
@@ -0,0 +1,97 @@
+<?php
+
+use LeanCloud\Engine\Cloud;
+
+class CloudTest extends PHPUnit_Framework_TestCase {
+    public function testFunctionWithoutArg() {
+        Cloud::define("hello", function($params, $user) {
+            return "hello";
+        });
+
+        $result = Cloud::runFunc("hello", array(), null);
+        $this->assertEquals("hello", $result);
+    }
+
+    public function testFunctionWithArg() {
+        Cloud::define("sayHello", function($params, $user) {
+            return "hello {$params['name']}";
+        });
+
+        $result = Cloud::runFunc("sayHello", array("name" => "alice"), null);
+        $this->assertEquals("hello alice", $result);
+    }
+
+    public function testClassHook() {
+        forEach(array("beforeSave", "afterSave",
+                      "beforeUpdate", "afterUpdate",
+                      "beforeDelete", "afterDelete") as $hookName) {
+            $count = 42;
+            call_user_func(
+                array("LeanCloud\Engine\Cloud", $hookName),
+                "TestObject",
+                function($obj, $user) use (&$count) {
+                    $count += 1;
+                }
+            );
+            Cloud::runHook("TestObject", $hookName, null, null);
+            $this->assertEquals(43, $count);
+        }
+    }
+
+    public function testOnVerifiedHook() {
+        // use a closure to ensure hook being executed
+        $count = 42;
+        Cloud::onVerified("sms", function($user) use (&$count) {
+            $count += 1;
+        });
+        Cloud::runOnVerified("sms", null);
+        $this->assertEquals(43, $count);
+    }
+
+    public function testOnLogin() {
+        $count = 42;
+        Cloud::onLogin(function($user) use (&$count) {
+            $count += 1;
+        });
+        Cloud::runOnLogin(null);
+        $this->assertEquals(43, $count);
+    }
+
+    public function testOnInsight() {
+        $count = 42;
+        Cloud::onInsight(function($job) use (&$count) {
+            $count += 1;
+        });
+        Cloud::runOnInsight(null);
+        $this->assertEquals(43, $count);
+    }
+
+    public function testAfterSave() {
+        $count = 42;
+        Cloud::afterSave("TestObject", function($obj, $user) use (&$count) {
+            $count += 1;
+        });
+        Cloud::runHook("TestObject", "afterSave", null, null);
+        $this->assertEquals(43, $count);
+    }
+
+    public function testBeforeUpdate() {
+        $count = 42;
+        Cloud::beforeUpdate("TestObject", function($obj, $user) use (&$count) {
+            $count += 1;
+        });
+        Cloud::runHook("TestObject", "beforeUpdate", null, null);
+        $this->assertEquals(43, $count);
+    }
+
+    public function testAfterUpdate() {
+        $count = 42;
+        Cloud::afterUpdate("TestObject", function($obj, $user) use (&$count) {
+            $count += 1;
+        });
+        Cloud::runHook("TestObject", "afterUpdate", null, null);
+        $this->assertEquals(43, $count);
+    }
+
+}
+
