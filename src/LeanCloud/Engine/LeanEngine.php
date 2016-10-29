f2b034dd178c413d2b74d1b48bf750a5a2a04966
Juvenn Woo <machese@gmail.com>
2016-10-29 13:19:59 +0800

    (feat) Enable https redirect in stg mode close #53

--- src/LeanCloud/Engine/LeanEngine.php
+++ src/LeanCloud/Engine/LeanEngine.php
@@ -617,7 +617,7 @@ class LeanEngine {
     private function httpsRedirect() {
         $reqProto = $this->getHeaderLine("HTTP_X_FORWARDED_PROTO");
         if ($reqProto === "http" &&
-            getenv("LC_APP_ENV") === "production") {
+            in_array(getenv("LC_APP_ENV"), array("production", "stg"))) {
             $url = "https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
             $this->redirect($url);
         }
