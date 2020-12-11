918e5cae7ce64c6f500b4d30521ea07073c9ab8c
Jang Rush <weakish@gmail.com>
2020-12-11 20:40:10 +0800

    test(file): more rigid specified file key test
    - New intl. applications share a gluttony bucket.
    - Users may use `/` in the specified key.

--- test/MasterTest.php
+++ test/MasterTest.php
@@ -18,7 +18,6 @@ class MasterTest extends TestCase {
         $file = File::createWithData("test.txt", "Hello World!");
         $file->setKey("abc");
         $this->assertEquals("abc", $file->getKey());
-        // $this->setExpectedException("LeanCloud\CloudException", null, 603);
         $file->save();
         $this->assertNotEmpty($file->getObjectId());
         $this->assertNotEmpty($file->getName());
@@ -28,9 +27,13 @@ class MasterTest extends TestCase {
         $parsedUrl = parse_url($url);
         $path = $parsedUrl["path"];
         $oldSchoolAwsS3FileUrlPrefix = "/avos-cloud-";
+        $newAwsS3GluttonyUrlPrefix = "https://lc-gluttony";
         if (substr($path, 0, strlen($oldSchoolAwsS3FileUrlPrefix)) === $oldSchoolAwsS3FileUrlPrefix) {
-            $splitPath = explode("/", $path);
+            $splitPath = explode("/", $path, 3);
             $this->assertEquals("abc", $splitPath[2]);
+        } else if (substr($url, 0, strlen($newAwsS3GluttonyUrlPrefix)) === $newAwsS3GluttonyUrlPrefix) {
+            $gluttonyPath = "/" + substr(getenv("LEANCLOUD_APP_KEY"), 0, 12) + "/abc";
+            $this->assertEquals($gluttonyPath, $path);
         } else {
             $this->assertEquals("/abc", $path);
         }
